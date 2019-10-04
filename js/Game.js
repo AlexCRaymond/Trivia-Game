6a1db3f9d3c35c8f67bf35afc0cc4c7f9504568a
Alex Raymond <alexraymond@Alexs-MacBook-Air.local>
2019-10-04 16:35:39 -0700

    Setup complete up to difficutly choosing of each category

---
+++ js/Game.js
@@ -0,0 +1,37 @@
+$('#start-button').on('click', () => {
+    $('.start-page').attr('class','hide')
+    $('.category-choosing-page').attr('class','start-page')
+})
+
+$('#sports-button').on('click', () => {
+    $('.start-page').attr('class','hide')
+    $('.sports-difficulty-choosing-page').attr('class','sports-difficulty-level-choice')
+})
+
+$('#entertainment-button').on('click', () => {
+    $('.start-page').attr('class','hide')
+    $('.entertainment-difficulty-choosing-page').attr('class', 'entertainment-difficulty-level-choice')
+})
+
+$('#music-button').on('click', () => {
+    $('.start-page').attr('class','hide')
+    $('.music-difficulty-choosing-page').attr('class', 'music-difficulty-level-choice')
+})
+
+$('#geography-button').on('click', () => {
+    $('.start-page').attr('class','hide')
+    $('.geography-difficulty-choosing-page').attr('class', 'geography-difficulty-level-choice')
+})
+
+// const SportsEasyQuestions = [
+//     {
+//         question: "How many teams are in the NFL?",
+//         answers: ['10', '20', '28', '32'],
+//         rightAnswer: 3
+//     }
+// ]
+
+// let score = 0;
+
+// for (let i = 0; i < SportsEasyQuestions.length; i++) {
+// };
