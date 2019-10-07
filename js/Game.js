99750187e4e028940a5d9acc9c2f80806a59ab82
Alex Raymond <alexraymond@Alexs-MacBook-Air.local>
2019-10-07 11:58:21 -0700

    Can navigate to game page with timer (doesn't stop the game) starting questions

--- js/Game.js
+++ js/Game.js
@@ -25,41 +25,48 @@ $('#geography-button').on('click', () => {
 
 $('#sports-easy-button').on('click', () => {
     $('.sports-difficulty-level-choice').attr('class','hide')
-    $('.sports-easy-questions-page').attr('class', 'sports-difficulty-level-choice')
+    $('.get-ready-page').attr('class', 'sports-difficulty-level-choice')
+})
+
+$('#start-button').on('click', () => {
+    $('.sports-difficulty-level-choice').attr('class', 'hide')
+    $('.game-page').attr('class', 'game-page')
 })
 
 
+const game = {
+    time: 10,
+    setTimer() {
+        const timer = setInterval(() => {
+            const $clock = $('#clock')
+            this.time--
+            $clock.text(`Timer: ${this.time}s`)
+            if(this.time === 0){
+                console.log('hi')
+                clearInterval(timer)
+            }
+        }, 1000)
+    }
+}
+
+$('#start-button').on('click', (e) => {
+    if($(e.target).text() === "START"){
+        game.setTimer()  
+    }
+})
 
 
+const SportsEasyQuestions = [
+    
+    {
+        question: "How many teams are in the NFL?",
+        answers: ['10', '20', '28', '32'],
+        rightAnswer: 3
+    }
+]
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const SportsEasyQuestions = [
-//     {
-//         question: "How many teams are in the NFL?",
-//         answers: ['10', '20', '28', '32'],
-//         rightAnswer: 3
-//     }
-// ]
-
-// let score = 0;
-
-// for (let i = 0; i < SportsEasyQuestions.length; i++) {
-// };
-
-// SportsEasyQuestions[0].question // accessing my first question
+let score = 0;
+for (let i = 0; i < SportsEasyQuestions.length; i++) {
+};
+SportsEasyQuestions[0].question // accessing my first question
