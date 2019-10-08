c774a3e4f15af492b1e405ac1b04f6a957f32c14
Alex Raymond <alexraymond@Alexs-MacBook-Air.local>
2019-10-08 10:23:19 -0700

    have addQuestions function - beginning stages of making dynamic

--- js/Game.js
+++ js/Game.js
@@ -3,33 +3,53 @@ $('#lets-go-button').on('click', () => {
     $('.category-choosing-page').attr('class','start-page')
 })
 
-$('#sports-button').on('click', () => {
+$('.categories').on('click',(e) => {
+    game.category = $(e.target).text().toLowerCase()
     $('.start-page').attr('class','hide')
-    $('.sports-difficulty-choosing-page').attr('class','sports-difficulty-level-choice')
+    $('.difficulty-choosing-page').attr('class','difficulty-level-choice')
+    $('.difficulty-header').text(`Please select a difficulty level for the upcoming ${game.category} questions:`)
+
 })
 
-$('#entertainment-button').on('click', () => {
-    $('.start-page').attr('class','hide')
-    $('.entertainment-difficulty-choosing-page').attr('class', 'entertainment-difficulty-level-choice')
-})
 
-$('#music-button').on('click', () => {
-    $('.start-page').attr('class','hide')
-    $('.music-difficulty-choosing-page').attr('class', 'music-difficulty-level-choice')
-})
 
-$('#geography-button').on('click', () => {
-    $('.start-page').attr('class','hide')
-    $('.geography-difficulty-choosing-page').attr('class', 'geography-difficulty-level-choice')
-})
+// $('#sports-button').on('click', () => {
+//     $('.start-page').attr('class','hide')
+//     $('.difficulty-choosing-page').attr('class','difficulty-level-choice')
+//     game.category = 'sports'
+// })
 
-$('#sports-easy-button').on('click', () => {
-    $('.sports-difficulty-level-choice').attr('class','hide')
-    $('.get-ready-page').attr('class', 'sports-difficulty-level-choice')
-})
+// $('#entertainment-button').on('click', () => {
+//     $('.start-page').attr('class','hide')
+//     $('.difficulty-choosing-page').attr('class', 'difficulty-level-choice')
+//     game.category = 'entertainment'
+// })
 
-$('#start-button').on('click', () => {
-    $('.sports-difficulty-level-choice').attr('class', 'hide')
+// $('#music-button').on('click', () => {
+//     $('.start-page').attr('class','hide')
+//     $('.difficulty-choosing-page').attr('class', 'difficulty-level-choice')
+//     game.category = 'music'
+// })
+
+// $('#geography-button').on('click', () => {
+//     $('.start-page').attr('class','hide')
+//     $('.difficulty-choosing-page').attr('class', 'difficulty-level-choice')
+//     game.category = 'geography'
+// })
+
+// $('#sports-easy-button').on('click', () => {
+//     $('.sports-difficulty-level-choice').attr('class','hide')
+//     $('.get-ready-page').attr('class', 'sports-difficulty-level-choice')
+// })
+
+// $('#start-button').on('click', () => {
+//     $('.sports-difficulty-level-choice').attr('class', 'hide')
+//     $('.game-page').attr('class', 'game-page')
+// })
+
+$(".difficulty-level-choice").on('click', (e) => {
+    game.difficulty = $(e.target).text().toLowerCase()
+    $('.difficulty-level-choice').attr('class', 'hide')
     $('.game-page').attr('class', 'game-page')
 })
 
@@ -38,7 +58,8 @@ const game = {
     time: 10,
     timer: null,
     score: 0,
-
+    category: null,
+    difficulty: null,
     setTimer() {
         this.timer = setInterval(() => {
             const $clock = $('#clock')
@@ -59,40 +80,21 @@ $('#start-button').on('click', (e) => {
 })
 
 const spentQuestions = []
-const SportsEasyQuestions = [
-    {
-        question: "How many teams are in the NFL?",
-        answers: ['10', '20', '28', '32'],
-        rightAnswer: 3
-    },
 
-    {
-        question: "What does the term 'birdie' mean in golf?",
-        answers: ['One under par', 'One over par', 'Two under par', 'You killed a bird'],
-        rightAnswer: 0
-    },
-
-    {
-        question: "Which of the following athletes is incorrectly matched with his sport?",
-        answers: ['Michael Jordan - Basketball','Wayne Gretzky - Ice Hockey',
-            'Babe Ruth - Baseball','Muhammad Ali - Football'],
-        rightAnswer: 3
-    },
-]
 
 function addQuestion() {
-    
-    if(SportsEasyQuestions.length > 0) {
-        const random = Math.floor(Math.random() * SportsEasyQuestions.length)
-        $('#question-box').text(SportsEasyQuestions[random].question)
+    // if(questions[game.category][game.difficulty].length > 0)
+    if(sportsQuestions.easy.length > 0) {
+        const random = Math.floor(Math.random() * sportsQuestions.easy.length)
+        $('#question-box').text(sportsQuestions.easy[random].question)
         
-        for(let i = 0; i < SportsEasyQuestions[random].answers.length; i++) {
-            let li = `<li><button>${SportsEasyQuestions[random].answers[i]}</button></li>`
+        for(let i = 0; i < sportsQuestions.easy[random].answers.length; i++) {
+            let li = `<li><button>${sportsQuestions.easy[random].answers[i]}</button></li>`
             $('#answers').append(li)
         }
         
         $('button').on('click', e => {
-            if(SportsEasyQuestions[random].answers.indexOf(e.target.innerText) === SportsEasyQuestions[random].rightAnswer) {
+            if(sportsQuestions.easy[random].answers.indexOf(e.target.innerText) === sportsQuestions.easy[random].rightAnswer) {
                 game.score+=1
                 $('#score').text(`Score: ${game.score}`)
                 console.log('right!')
@@ -102,8 +104,8 @@ function addQuestion() {
             game.time = 10;
             $("#clock").text(`Timer: ${game.time}s`);
             game.setTimer()
-            spentQuestions.push(SportsEasyQuestions[random].question)
-            SportsEasyQuestions.splice(random, 1)
+            spentQuestions.push(sportsQuestions.easy[random].question)
+            sportsQuestions.easy.splice(random, 1)
             addQuestion()
             
         })
@@ -111,20 +113,90 @@ function addQuestion() {
     }
 }
 
+const questions = {
+    sports: {
+        easy: [
+            {
+                question: "How many teams are in the NFL?",
+                answers: ['10', '20', '28', '32'],
+                rightAnswer: 3
+            },
+        ]
+    }
+}
 
+const sportsQuestions = {
+    easy: [
+        {
+            question: "How many teams are in the NFL?",
+            answers: ['10', '20', '28', '32'],
+            rightAnswer: 3
+        },
+    
+        {
+            question: "What does the term 'birdie' mean in golf?",
+            answers: ['One under par', 'One over par', 'Two under par', 'You killed a bird'],
+            rightAnswer: 0
+        },
+    
+        {
+            question: "Which of the following athletes is incorrectly matched with his sport?",
+            answers: ['Michael Jordan - Basketball','Wayne Gretzky - Ice Hockey',
+                'Babe Ruth - Baseball','Muhammad Ali - Football'],
+            rightAnswer: 3
+        },
 
+        {
+            question: "Who has won more tennis grand slam titles, Venus Williams or Serena Williams?",
+            answers: ['Venus Williams','Serena Williams'],
+            rightAnswer: 1
+        
+        },
+    ],
 
+    medium: [
+        {
+            question: "How many NBA championships did Michael Jordan win with the Chicago Bulls?",
+            answers: ['4', '5', '6', '8'],
+            rightAnswer: 2
+        },
 
+        {
+            question: "Which golf tournament did Tiger Woods win by 12 strokes in 1997 to record his first major championship win?",
+            answers: ['The Masters', 'The Open', 'The Players Championship', 'PGA Championship'],
+            rightAnswer: 0
+        },
 
+        {
+            question: "What's the name of the 15 year old female breakout player of 2019?",
+            answers: ['Sloane Stephens', 'Naomi Osaka', 'Coco Gauff', 'Simona Halep'],
+            rightAnswer: 2
+        }
 
-// const sportsQuestions = {
-//     easy: [
-//         {
+    ],
 
-//         }
-//     ]
-// }
+    hard: [
+        
+        {
+            question: "Which is the only American Football team to go a whole season undefeated, including the Super Bowl?",
+            answers: ['Baltimore Colts', 'Miami Dolphins', 'St. Louis Rams', 'Tennessee Oilers'],
+            rightAnswer: 1
+        },
+
+        {
+            question: "Which is the only team to play in every soccer World Cup tournament?",
+            answers: ['Brazil', 'Argentina', 'Spain', 'Liverpool'],
+            rightAnswer: 0
+        },
+
+        {
+            question: "Which American Football team won the first two Super Bowls (in 1967 and 1968)?",
+            answers: ['Dallas Cowboys', 'Philadelphia Eagles', 'Cleveland Browns', 'Green Bay Packers'],
+            rightAnswer: 3
+        }
+    ]
+}
 
 // const level = 'easy'
 
-// sportsQuestions[level]
+// sportsQuestions.easy.easy[level]
